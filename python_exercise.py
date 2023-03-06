cbddc116af2de2fc1ec35e1713c0e24dc6f477c1
daniel.kohav <danielkohav2@gmail.com>
2023-03-06 10:35:11 +0200

    everything works ok

--- python_exercise.py
+++ python_exercise.py
@@ -6,10 +6,12 @@ def main():
     parameters = sys.argv
 
     # input validation - only 1 numeric argument
+
+    # if no argument passed, read from input file
     if len(parameters) == 1:
         file_path = os.path.expanduser("~") + "/python_exercise_input.txt"
         with open(file_path, 'r') as file:
-            num = file.read().rstrip()
+            num = int(file.read().rstrip())
 
     elif len(parameters) != 2:
         print("Error: Please provide only 1 input")
@@ -22,12 +24,13 @@ def main():
     else:
         num=int(parameters[1])
 
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', filename='/var/log/python_exercise.log', level=logging.INFO)
+    # set logging with timestamps
+    logging.basicConfig(format= '%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', filename='/var/log/python_exercise.log', level=logging.INFO)
     print(num)
-    for i in range(1, num):
+    for i in range(1, num+1):
+        # all the natural dividers of the number 
         if num % i == 0:
             logging.info(i)
-            print(i)
 
 
 if __name__ == '__main__':
