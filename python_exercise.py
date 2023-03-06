cdba9c921527117f4e40227ea45f92ed068b06c0
daniel.kohav <danielkohav2@gmail.com>
2023-03-06 09:59:41 +0200

    reading from file in case of no input implemented

--- python_exercise.py
+++ python_exercise.py
@@ -1,26 +1,32 @@
 import sys
 import logging
+import os
 
 def main():
     parameters = sys.argv
 
     # input validation - only 1 numeric argument
     if len(parameters) == 1:
-        print("Error: No input")
-        exit(1)
-    if len(parameters) != 2:
+        file_path = os.path.expanduser("~") + "/python_exercise_input.txt"
+        with open(file_path, 'r') as file:
+            num = file.read().rstrip()
+
+    elif len(parameters) != 2:
         print("Error: Please provide only 1 input")
         exit(1)
-    if not parameters[1].isnumeric():
+
+    elif not parameters[1].isnumeric():
         print("Error: Parameter is not a number")
         exit(1)
 
-    logging.basicConfig(filename='myapp.log', level=logging.INFO)
-    num=int(parameters[1])
-    print("arg: ", num)
+    else:
+        num=int(parameters[1])
 
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', filename='/var/log/python_exercise.log', level=logging.INFO)
+    print(num)
     for i in range(1, num):
         if num % i == 0:
+            logging.info(i)
             print(i)
 
 
