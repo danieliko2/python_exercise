ec30e1224a2504dd452b122129f532d297b4f176
daniel.kohav <danielkohav2@gmail.com>
2023-03-06 09:26:16 +0200

    input improved

--- python_exercise.py
+++ python_exercise.py
@@ -3,10 +3,15 @@ import sys
 num = sys.argv[1]
 if len(sys.argv) == 1:
     print("no input")
-
+    exit(1)
+if len(sys.argv) != 2:
+    print("please provide only 1 input")
+    exit(1)
 if num.isnumeric():
     print("num")
 else:
     print("not a number")
+    exit(1)
+
 print("arg: ", sys.argv[1])
 
